c531eb7f43a1270eba6c8d21ece5d07ea8174dcf
ThinhNguyen <79948452+NguyenVanThinh2000@users.noreply.github.com>
2024-10-02 23:49:47 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://local.thinhnguyen.me:5174'],
+    origin: ['http://local.thinhnguyen.me:5174', 'https://thinhnvt.vercel.app/'],
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
