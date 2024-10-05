d466dfac98b6503d4f4bf1c3f15c679e2d4d4f92
ThinhNguyen <79948452+NguyenVanThinh2000@users.noreply.github.com>
2024-10-05 20:10:37 +0700

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 export const corsOptions: CorsOptions = {
   origin: [
     'http://local.thinhnguyen.me:5174',
+    'http://localhost:5174',
     'https://thinhnvt.vercel.app',
     'https://thinhnguyen-nguyenvanthinh2000s-projects.vercel.app/',
     'https://thinhnguyen-git-dev-nguyenvanthinh2000s-projects.vercel.app/',
