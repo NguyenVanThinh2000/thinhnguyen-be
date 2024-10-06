6336828959acc1ae6ea6ce10711f027295d20cfe
thinhnguyen <nguyenvanthinh2701@gmail.com>
2024-10-06 18:41:53 +0700

    update

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,6 +5,7 @@ export const corsOptions: CorsOptions = {
     'http://local.thinhnguyen.me:5174',
     'http://localhost:5174',
     'https://thinhnvt.vercel.app',
+    'https://thoanthinh-wedding.vercel.app',
     'https://thinhnguyen-nguyenvanthinh2000s-projects.vercel.app/',
     'https://thinhnguyen-git-dev-nguyenvanthinh2000s-projects.vercel.app/',
   ],
